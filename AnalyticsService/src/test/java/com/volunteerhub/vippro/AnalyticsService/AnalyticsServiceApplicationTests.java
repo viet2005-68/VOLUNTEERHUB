8003e42f56c6e79ddfe4f94447d09aa116bb4eb6
tungluuquang <tungq2005@gmail.com>
2026-04-23 17:12:43 +0700

    feat: init Analytics Service

---
+++ AnalyticsService/src/test/java/com/volunteerhub/vippro/AnalyticsService/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteerhub.vippro.AnalyticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
