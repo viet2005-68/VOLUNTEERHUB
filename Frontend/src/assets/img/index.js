7fe570c8383710f60f1807109b2a7e39b70c86e5
tranthanhvan235 <vanvanvan2305@gmail.com>
2026-04-26 10:45:21 +0700

    feat: add LandingPage component with responsive design and animations

---
+++ Frontend/src/assets/img/index.js
@@ -0,0 +1,5 @@
+import landing1 from './landing1.png'
+import voluteerTree from './volunteering-tree.jpg'
+import futureVolunteer from './Future-of-Volunteering.jpg'
+import Logo from './logo.png'
+export { landing1, voluteerTree, futureVolunteer, Logo }
