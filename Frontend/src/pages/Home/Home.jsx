a7b1d2c95200a3fe2b84a7ed23ee95db699e7575
tranthanhvan235 <vanvanvan2305@gmail.com>
2026-04-27 10:30:57 +0700

    feat: implement Home component with dashboard layout and activity sections for enhanced user experience

---
+++ Frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import DashBoardOverview from "../../components/Dashboard/DashBoardOverview";
+import ModalConfirm from "../../components/Modal/ModalConfirm";
+import DashboardLayout from "../DashBoard/DashboardLayout";
+
+import QuickActionsUser from "../../components/QuickActionButton/QuickActionsUser";
+import UpcomingEvents from "../../components/ModalActivity/UpcomingEvents";
+import RecentActivity from "../../components/ModalActivity/RecentActivity";
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen flex-col gap-10">
+            <DashboardLayout />
+            <DashBoardOverview />
+            <QuickActionsUser />
+            <div className="w-full">
+                <div className="grid grid-cols-2 gap-10 max-lg:block">
+                    <UpcomingEvents className="basis-1/2" />
+                    <RecentActivity className="basis-1/2" />
+                </div>
+            </div>
+        </div>
+    );
+}
