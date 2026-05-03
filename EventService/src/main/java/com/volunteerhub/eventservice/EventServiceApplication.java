3a20793cd6e5478762ae198701713f89c5918700
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2026-05-03 13:13:03 +0700

    Init Event Service

---
+++ EventService/src/main/java/com/volunteerhub/eventservice/EventServiceApplication.java
@@ -0,0 +1,22 @@
+package com.volunteerhub.eventservice;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableMethodSecurity
+@EnableCaching
+public class EventServiceApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
