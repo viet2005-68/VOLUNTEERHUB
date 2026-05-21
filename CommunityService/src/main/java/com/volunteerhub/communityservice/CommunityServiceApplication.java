e7e5dc255a1eb844c54da1897e8ce57a49f655f4
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2026-05-21 11:29:36 +0700

    configure spring boot service baseline

--- CommunityService/src/main/java/com/volunteerhub/communityservice/CommunityServiceApplication.java
+++ CommunityService/src/main/java/com/volunteerhub/communityservice/CommunityServiceApplication.java
@@ -1,12 +1,21 @@
 package com.volunteerhub.communityservice;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMethodSecurity
+@EnableFeignClients
 public class CommunityServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(CommunityServiceApplication.class, args);
 	}
 
