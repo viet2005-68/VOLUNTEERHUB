d4440f854dd9a80038c4f7448073b16aa6c36007
tungluuquang <tungq2005@gmail.com>
2026-05-21 07:45:03 +0700

    feat: initialize aggregation service for microservices orchestration

---
+++ AggregationService/src/main/java/com/volunteerhub/AggregationService/AggregationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.volunteerhub.AggregationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregationServiceApplication.class, args);
+	}
+
+}
