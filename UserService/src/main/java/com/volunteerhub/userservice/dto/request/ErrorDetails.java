545e9e11bcc4da3b4d489964fd41dc64000faa56
VietPhan <phanvanviet70@gmail.com>
2026-05-21 17:57:51 +0700

    feat(user-service): add exception advice

---
+++ UserService/src/main/java/com/volunteerhub/userservice/dto/request/ErrorDetails.java
@@ -0,0 +1,12 @@
+package com.volunteerhub.userservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private String message;
+}
