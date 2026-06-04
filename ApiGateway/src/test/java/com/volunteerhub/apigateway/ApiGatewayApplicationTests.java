4eea77bf4f228657498cc3ef8506194690a2d7d8
VietPhan <phanvanviet70@gmail.com>
2026-06-04 19:34:48 +0700

    Run backend tests in CI

--- ApiGateway/src/test/java/com/volunteerhub/apigateway/ApiGatewayApplicationTests.java
+++ ApiGateway/src/test/java/com/volunteerhub/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,19 @@ package com.volunteerhub.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"server.port=0",
+		"spring.data.redis.host=localhost",
+		"spring.data.redis.port=6379",
+		"spring.data.redis.password=",
+		"spring.cloud.discovery.enabled=false",
+		"eureka.client.enabled=false",
+		"app.cors.allowed-origins=http://localhost:30080",
+		"spring.security.oauth2.google.issuer=https://accounts.google.com",
+		"spring.security.oauth2.google.jwk-uri=https://www.googleapis.com/oauth2/v3/certs",
+		"spring.security.oauth2.volunteerhub.issuer=http://localhost:7070",
+		"spring.security.oauth2.volunteerhub.jwk-uri=http://localhost:7070/oauth2/jwks"
+})
 class ApiGatewayApplicationTests {
 
 	@Test
--- EventService/src/test/java/com/volunteerhub/eventservice/EventServiceApplicationTests.java
+++ EventService/src/test/java/com/volunteerhub/eventservice/EventServiceApplicationTests.java
@@ -1,10 +1,36 @@
 package com.volunteerhub.eventservice;
 
+import com.google.cloud.storage.Storage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"server.port=0",
+		"spring.datasource.url=jdbc:h2:mem:eventservice_test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+		"spring.data.redis.host=localhost",
+		"spring.data.redis.port=6379",
+		"spring.data.redis.password=",
+		"spring.rabbitmq.listener.simple.auto-startup=false",
+		"spring.rabbitmq.listener.direct.auto-startup=false",
+		"spring.cloud.discovery.enabled=false",
+		"eureka.client.enabled=false",
+		"rabbitmq.exchange.notification=notification-exchange",
+		"rabbitmq.exchange.event=event-exchange",
+		"rabbitmq.routingKey.notification=notification.event",
+		"rabbitmq.routingKey.event=event.event",
+		"firebase.projectId=",
+		"firebase.storage.bucket=",
+		"firebase.credentials.location=",
+		"firebase.credentials.base64="
+})
 class EventServiceApplicationTests {
+
+	@MockBean
+	private Storage storage;
 
 	@Test
 	void contextLoads() {
